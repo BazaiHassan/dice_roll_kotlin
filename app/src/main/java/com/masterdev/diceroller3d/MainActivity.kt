dd8eeb5c73a6a717eab81cec87bada72f2f02951
HBazai <bazaee.hassan@gmail.com>
2021-10-15 16:10:04 +0330

    Dice Roll Kotlin

---
+++ app/src/main/java/com/masterdev/diceroller3d/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.masterdev.diceroller3d
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var rollBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViews()
+        listener()
+
+    }
+
+    private fun listener() {
+        rollBtn.text = "Let's Roll"
+        rollBtn.setOnClickListener {
+            diceRoll()
+        }
+    }
+
+    private fun findViews() {
+        rollBtn = findViewById(R.id.btn_roll)
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun diceRoll() {
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
